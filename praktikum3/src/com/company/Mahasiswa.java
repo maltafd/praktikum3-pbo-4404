2a972021f48a4d5570c73416392a3e71f31b3069
maltafd <71365334+maltafd@users.noreply.github.com>
2021-10-13 20:40:44 +0700

    Add files via upload

---
+++ praktikum3/src/com/company/Mahasiswa.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Mahasiswa {
+    private String nim;
+    private String nama;
+
+    public Mahasiswa(String nim, String nama) {
+        this.nim = nim;
+        this.nama = nama;
+    }
+
+    public String getnim() {
+        return nim;
+    }
+
+    public String getnama() {
+        return nama;
+    }
+}
